2fe042e4cb35b34e69fb6467b4a1d7726bd4cde7
kaybrian <kayonbrian@gmail.com>
2020-05-18 20:52:57 +0300

    getting the user info from the website

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+var updateButton = document.getElementsByClassName('update-cart')
+
+
+for (var i = 0; i < updateButton.length; i++){
+    updateButton[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log("productId:",productId, 'action:',action)
+
+        console.log('User :', user)
+    })
+}
